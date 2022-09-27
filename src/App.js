d3bdb73ea2a37c66ce3fb88a514eb61a1abc2d28
Alexander Gustafsson Flink <Alexander.GustafssonFlink@gmail.com>
2022-09-27 14:38:51 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import NavBar from "./components/navbar/navbar.js";
+import Home from "./views/home/home.js";
+import About from "./views/about/about.js";
+import Footer from "./components/footer/footer.js"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  status: {
+    danger: '#e53e3e',
+  },
+  palette: {
+    primary: {
+        main: "#533483",
+    //   main: '#0971f1',
+      darker: '#053e85',
+    },
+    neutral: {
+      main: '#64748B',
+      contrastText: '#fff',
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+        <div className="App">
+            <Router>
+            <NavBar />
+                <div>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/about" element={<About />} />
+                    </Routes>
+                </div>
+            </Router>
+            <Footer />
+        </div>
+    </ThemeProvider>
   );
 }
+
+
 
 export default App;
---
+++ src/components/footer/footer.js
@@ -0,0 +1,28 @@
+import React from "react";
+import './footer.css';
+import logo from '../../images/logo.svg';
+
+
+function Footer() {
+  return (
+        <div className="footer">
+            <div className="container footer-wrapper">
+                <img src={logo} alt="" />
+                <div className="company-info">
+                    <div className="address">
+                        <p> Fribergsgatan 6</p>
+                        <p> 412 60 Göteborg</p>
+                    </div>
+                    <div className="contact">
+                        <p>0708 63 86 11</p>
+                        <a href="mailto:hola@yaada.se"><p>hola@yaada.se</p></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+  );
+}
+
+
+
+export default Footer;
